1250c3b36d0eba68cf5213bb9638c13733ff44a7
TuanNguyen <nguyenhuynhanhtuan2407@gmail.com>
2023-10-13 17:36:45 +0700

    Add MyViewModel

---
+++ AptitudeTest.WebApp/ViewModels/MyViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AptitudeTest.WebApp.ViewModels
+{
+    public class MyViewModel
+    {
+        public String myName = "Nguyen Huynh Anh Tuan";
+        public int myAge = 23;
+        public String myEmail = "nguyenhuynhanhtuan2407@gmail.com";
+        public int myNumberId = 24;
+    }
+}
